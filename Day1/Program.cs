b2400e1bf85958b4b7742ebbdfc4d859336934c9
Mike Bucher <mike.bucher@digitecgalaxus.ch>
2024-12-02 12:43:17 +0100

    day 1

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿var input = File.ReadAllLines(@"../../../input.txt");
+var result = 0;
 
-Console.WriteLine("Hello, World!");
+var left = new List<int>();
+var right = new List<int>();
+foreach (var line in input)
+{
+    var parts = line.Split("   ");
+    left.Add(int.Parse(parts[0]));
+    right.Add(int.Parse(parts[1]));
+}
+
+//Part 1
+left.Sort();
+right.Sort();
+
+for (var i = 0; i < left.Count; i++)
+{
+    result += Math.Abs(left[i] - right[i]); 
+}
+
+Console.WriteLine($"Part1: {result}");
+
+//Part 2
+var result2 = (from number in left let count = right.Count(x => x.Equals(number)) select count * number).Sum();
+
+Console.WriteLine($"Part2: {result2}");
